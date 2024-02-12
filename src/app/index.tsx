16cfde7988a2abd7cd5bf5b21fafe95ff4f6d8a9
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-02-11 22:42:08 -0300

    config: Setting up React Native Application with TailwindCSS

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>React Native</Text>
+        </View>
+    )
+}
