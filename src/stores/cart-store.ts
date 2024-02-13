9f69ec24e661c1ab7a0550e99ec8659ef123a537
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-02-13 18:50:57 -0300

    feat: 01 - Implementing add, remove & clear functionalities 02 - Implementing data persist with AsyncStorage

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
-import { ProductProps } from "@/utils/data/products";
-import { create } from "zustand";
+import { ProductProps } from "@/utils/data/products"
+import { create } from "zustand"
+import { createJSONStorage, persist } from 'zustand/middleware'
 import * as cartInMemory from './helpers/cart-in-memory'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export type BagProps = ProductProps & {
     quantity: number
@@ -9,11 +11,21 @@ export type BagProps = ProductProps & {
 type StateProps = {
     bag: BagProps[]
     add: (product: ProductProps) => void
+    remove: (productId: string) => void
+    clear: () => void
 }
 
-export const useCartStore = create<StateProps>((set) => ({
-    bag: [],
-    add: (product: ProductProps) => set((state) => ({
-        bag: cartInMemory.add(state.bag, product)
+export const useCartStore = create(
+    persist<StateProps>((set) => ({
+        bag: [],
+        add: (product: ProductProps) => set((state) => ({
+            bag: cartInMemory.add(state.bag, product)
+        })),
+        remove: (productId: string) => set((state) => ({
+            bag: cartInMemory.remove(state.bag, productId)
+        })),
+        clear: () => set({ bag: [] })
+    }), {
+        name: "nlw-expert:cart",
+        storage: createJSONStorage(() => AsyncStorage)
     }))
-}))
